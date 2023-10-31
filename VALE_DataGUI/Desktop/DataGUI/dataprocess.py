5317ea4a5ba0d82160d15a305c95f049bfa63eec
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2023-10-31 16:35:37 +0700

    GUI Data

--- VALE_DataGUI/Desktop/DataGUI/dataprocess.py
+++ VALE_DataGUI/Desktop/DataGUI/dataprocess.py
@@ -3,11 +3,15 @@
 import numpy as np
 
 from matplotlib import style
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import matplotlib.animation as animation	
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 
 import PyQt6.QtWidgets as QWidgets
+
+import scipy.io as sio
+import pandas as pd
 
 class DataProcess():
     
@@ -37,5 +41,46 @@ class DataProcess():
         
         return hbox
         
+    def exampleMillPlot(self):
+        """
+        Milling Data Source:
+        https://www.nasa.gov/intelligent-systems-division/discovery-and-systems-health/pcoe/pcoe-data-set-repository/
+        """
+        
+        m = sio.loadmat('dataset/mill/mill.mat',struct_as_record=True)
+        data = m['mill']
+        l = data.dtype.names
+        df_labels = pd.DataFrame()
+        for i in range(7):
+            # list for storing the label data for each field
+            x = []
+    
+            # iterate through each of the unique cuts
+            for j in range(167):
+                x.append(data[0,j][i][0][0])
+            x = np.array(x)
+            df_labels[str(i)] = x
+
+        # add column names to the dataframe
+        df_labels.columns = l[0:7]
+    
+        # create a column with the unique cut number
+        df_labels['cut_no'] = [i for i in range(167)]
+        
+        fig, ax = plt.subplots()
+
+        ax.plot(data[0,166]['smcAC'],'g-',label='smcAC')
+        ax.plot(data[0,166]['smcDC'],color='orange',label='smcDC')
+        plt.legend()
+        
+        style.use('ggplot')
+        self.canvas = FigureCanvas(fig)
+        self.canvas.draw()
+        
+        hbox = QWidgets.QHBoxLayout()
+        hbox.addWidget(self.canvas)
+        
+        return hbox
+
     def graphupdate(self,args):
         self.line.set_data(self.X,self.Y)
--- VALE_DataGUI/Desktop/DataGUI/ui/mainwindow.py
+++ VALE_DataGUI/Desktop/DataGUI/ui/mainwindow.py
@@ -8,17 +8,20 @@ import sys
 import numpy as np
 
 from PyQt6.QtCore import PYQT_VERSION_STR
+import PyQt6.QtGui as QGui
 import PyQt6.QtWidgets as QWidgets
 from PyQt6.QtCore import pyqtSlot
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtWidgets import QMessageBox
 
-import matplotlib
-
 from .Ui_mainwindow import Ui_MainWindow
 
 sys.path.append('../')
 from dataprocess import DataProcess
+
+import matplotlib
+import scipy 
+import pandas
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -31,7 +34,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
     def gui_setup(self):        
         dp = DataProcess()
-        hbox = dp.examplePlot()
+        hbox = dp.exampleMillPlot()
         
         self.frmDataPlot.setLayout(hbox)
         
@@ -46,17 +49,36 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     @pyqtSlot()
     def on_actionAboutPython_triggered(self):
         pyver = "Python: " + sys.version + "\n\n"
-        npver = "NumPy: " + np.__version__ + "\n\n"
-        qtver = "PyQt: " + PYQT_VERSION_STR + "\n\n"
-        pltver = "Matplotlib: " +  matplotlib.__version__ + "\n\n"
+        npver = "NumPy: " + np.__version__ + "\n"
+        qtver = "PyQt: " + PYQT_VERSION_STR + "\n"
+        sciver = "SciPy: " + scipy.__version__ + "\n"
+        pndver = "Pandas: " + pandas.__version__ + "\n"
+        pltver = "Matplotlib: " +  matplotlib.__version__ + "\n"
         
-        strversion = pyver + npver + qtver + pltver
+        strversion = pyver + npver + qtver + sciver + pndver + pltver
         
         mbox = QMessageBox(self)
         mbox.setInformativeText(strversion)
         mbox.setWindowTitle("Version")
         mbox.exec()
-
+        
+    @pyqtSlot()
+    def on_actionAbout_triggered(self):
+        team = "Project Leader: Dr. Dhany Arifianto S.T M.Eng (dhany@ep.its.ac.id)\n"
+        team += "\n"
+        
+        team += "Programmers:\n"
+        team += "GUI Integrator: Achmadi ST MT (github.com/mekatronik-achmadi)\n"
+        team += "Data Analysis : M. Ammar A. ST MT (github.com/maasyraf-project)\n"
+        team += "Result Testing: A. Ainun Najib ST MT (github.com/iyyanf)\n"
+        team += "Network System: Aprianto Dwi P ST MT (github.com/arkiven4)\n"
+        
+        mbox = QMessageBox(self)
+        mbox.setInformativeText(team)
+        mbox.setWindowTitle("Team List")
+        mbox.setStyleSheet("QLabel{min-width: 400px;}");
+        mbox.exec()
+        
     @pyqtSlot()
     def on_actionExit_triggered(self):
-        self.close()
+        self.close()        
